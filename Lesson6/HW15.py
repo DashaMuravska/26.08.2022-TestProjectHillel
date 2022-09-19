89bfdf5a43037b0bbd4db2b9aa740a3d984a5fe4
DashaMuravska <muravska.daria7@gmail.com>
2022-09-19 18:28:47 +0300

    Lesson 6 (fixed)

--- Lesson6/HW15.py
+++ Lesson6/HW15.py
@@ -26,34 +26,40 @@
 #
 # Дополнить ведущими нулями можно с помощью метода zfill(2)
 
-n = int(0)
 # n = int(224930)
 # n = int(466289)
 # n = int(8639999)
 # n = int(22493)
 # n = int(7948799)
+n = int(input("Введите число от 0 до 8639999: "))
 
 days = int(n/(60*60*24))
 hours = int(n/(60*60) - (days*24))
 minutes = int((n/60) - (days*24+hours)*60)
 seconds = int(n - ((days*60*60*24) + (hours*60*60) + (minutes*60)))
 
-hours = str(hours)
-if len(hours) == 1:
-    hours = hours.zfill(2)
+if n == 0 or n <= 8639999:
+    hours = str(hours)
+    if len(hours) == 1:
+        hours = hours.zfill(2)
 
-minutes = str(minutes)
-if len(minutes) == 1:
-    minutes = minutes.zfill(2)
+    minutes = str(minutes)
+    if len(minutes) == 1:
+        minutes = minutes.zfill(2)
 
-seconds = str(seconds)
-if len(seconds) == 1:
-    seconds = seconds.zfill(2)
+    seconds = str(seconds)
+    if len(seconds) == 1:
+        seconds = seconds.zfill(2)
 
-if days == 0 or days > 1:
-    print(n, "->", days, "дней", hours + ":" + minutes + ":" + seconds)
-elif days == 1:
-    print(n, "->", days, "день", hours + ":" + minutes + ":" + seconds)
+    lst = [5, 6, 7, 8, 9]
+    if days == 0 or str(days)[-1] in lst:
+        print(n, "->", days, "дней", hours + ":" + minutes + ":" + seconds)
+    elif days == 1 or str(days)[-1] == '1':
+        print(n, "->", days, "день", hours + ":" + minutes + ":" + seconds)
+    else:
+        print(n, "->", days, "дня", hours + ":" + minutes + ":" + seconds)
 
+else:
+    print("Неверное число!")
 
 
